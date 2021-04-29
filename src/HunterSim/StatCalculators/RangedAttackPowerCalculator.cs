bda46a953e5b3928ff4e89e3901d1f1ab7c54366
Dylan Smith <dylan-smith@github.com>
2021-04-29 15:36:10 -0500

    implemented Suvival Instincts talent

--- src/HunterSim/StatCalculators/RangedAttackPowerCalculator.cs
+++ src/HunterSim/StatCalculators/RangedAttackPowerCalculator.cs
@@ -8,6 +8,11 @@ namespace HunterSim
         {
             var rangedAP = state.Config.Gear.GetAllGear().Sum(x => x.AttackPower + (x.Agility * 2));
             
+            if (state.Config.Talents.ContainsKey(Talent.SurvivalInstincts))
+            {
+                rangedAP *= 1 + (state.Config.Talents[Talent.SurvivalInstincts] * 0.02);
+            }
+
             return rangedAP;
         }
     }
