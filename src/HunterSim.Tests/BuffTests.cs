e6a87ae001acfb24a1ddf64e32c255517db907d2
Dylan Smith <dylan-smith@github.com>
2021-05-02 21:08:59 -0500

    ZG and songflower buff tests

--- src/HunterSim.Tests/BuffTests.cs
+++ src/HunterSim.Tests/BuffTests.cs
@@ -16,10 +16,36 @@ namespace HunterSim.Tests
             state.Config.PlayerSettings.Race = Race.Dwarf;
             state.Config.Buffs.Add(Buff.RallyingCryOfTheDragonSlayer);
 
-            // 121 base agi + buff
+            // 121 base agi + 140 buff
             Assert.AreEqual(242 + 140, RangedAttackPowerCalculator.Calculate(state));
-            // 121 base agi + 5% base crit + buff
+            // 121 base agi + 5% base crit + 5% buff
             Assert.AreEqual(0.023 + 0.05 + 0.05, CritChanceCalculator.Calculate(state), 0.001);
+        }
+
+        [TestMethod]
+        public void ZGBuff()
+        {
+            var state = new SimulationState();
+            state.Config = new SimulationConfig();
+            state.Config.PlayerSettings.Race = Race.Dwarf;
+            state.Config.Buffs.Add(Buff.SpiritOfZandalar);
+
+            // 121 base agi + 15% buff
+            Assert.AreEqual(121 * 1.15, AgilityCalculator.Calculate(state), 0.001);
+        }
+
+        [TestMethod]
+        public void SongflowerBuff()
+        {
+            var state = new SimulationState();
+            state.Config = new SimulationConfig();
+            state.Config.PlayerSettings.Race = Race.Dwarf;
+            state.Config.Buffs.Add(Buff.SongflowerSerenade);
+
+            // 121 base agi + 15 buff agi
+            Assert.AreEqual(121 + 15, AgilityCalculator.Calculate(state), 0.001);
+            // 121 base agi + 15 buff agi + 5% base crit + 5% buff
+            Assert.AreEqual(0.026 + 0.05 + 0.05, CritChanceCalculator.Calculate(state), 0.001);
         }
     }
 }
