d77d3e022a5fe0b5187b26548caa63ab6d88dc87
Dylan Smith <dylan-smith@github.com>
2021-05-05 02:10:24 -0500

    removed TODO

--- src/HunterSim/Events/AutoShotCastEvent.cs
+++ src/HunterSim/Events/AutoShotCastEvent.cs
@@ -10,7 +10,6 @@ namespace HunterSim
         public override void ProcessEvent(SimulationState state)
         {
             // TODO: Haste
-            // TODO: I don't think this is how auto-shot works, it should be a one-time 0.5s cast time, then immediate dmg, followed by weapon cooldown
             var autoShotSpeed = state.Config.Gear.Ranged.Speed;
             state.Events.Add(new AutoShotCompletedEvent(Timestamp + 0.5));
             state.Auras.Add(Aura.AutoShotOnCooldown);
