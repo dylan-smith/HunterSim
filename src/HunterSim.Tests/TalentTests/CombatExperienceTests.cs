99ba6215c1d66291118d552d5d343428ff7f347b
Dylan Smith <dylan-smith@github.com>
2021-07-24 15:52:34 -0500

    removed comment

--- src/HunterSim.Tests/TalentTests/CombatExperienceTests.cs
+++ src/HunterSim.Tests/TalentTests/CombatExperienceTests.cs
@@ -15,7 +15,5 @@ namespace HunterSim.Tests.TalentTests
             Assert.AreEqual(Constants.DRAENEI_AGI * 1.02, AgilityCalculator.Calculate(state), 0.00001);
             Assert.AreEqual(Constants.DRAENEI_INT * 1.06, IntellectCalculator.Calculate(state), 0.00001);
         }
-
-        // TODO: test for the increased crit on Kill Command once we have Kill Command implemented
     }
 }
